5fc665fd3531f94be49b9983846ddd2d2d6845f8
Mustafa Yıldırım <myldrm1025@gmail.com>
2023-07-15 18:59:47 +0300

    reafactor mail

--- app.js
+++ app.js
@@ -11,3 +11,15 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+function sendEmail() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    var mailtoLink = 'mailto:myldrm1025@gmail.com' +
+        '?subject=' + encodeURIComponent(subject) +
+        '&body=' + encodeURIComponent('Name: ' + name + '\nEmail: ' + email + '\n\n' + message);
+
+    window.location.href = mailtoLink;
+}
